dbf80657812a1d060390e02761d96178dfa1c54f
Aurel <157985436+GAurel396@users.noreply.github.com>
2024-04-20 03:06:57 +0200

    restoring after corruption of old git. added drawing function but needs fix for after-reset

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const board = document.querySelector(".board")
-const square = document.createElement("div")
+const draw_board = document.querySelector("#reset")
+const square_colour = ["red", "blue", "green" ,"yellow", "purple", "cyan", "brown", "pink", "grey", "black"]
 
-let row = 16
-let column = 16
+let row = 8
+let column = 8
 
+function drawBoard () {
+    console.log("drawing board")
+    let square = document.querySelectorAll(".square")
+        square.forEach (square => square.remove()) 
 
+    for (let i=0; i<column; i++) {
+        let draw_column_div = document.createElement("div")
+        draw_column_div.classList.add("columns_holder")
+            for (let e=0; e<row; e++) {
+            let draw_rows = document.createElement("div")
+            draw_rows.classList.add("square")
+            draw_column_div.appendChild(draw_rows)
+            }
+        board.appendChild(draw_column_div)  
+        
+        }
+    }
 
+    drawBoard()
+    let square = document.querySelectorAll(".square")
+    draw_board.addEventListener("click", drawBoard);
 
+    square.forEach(square => square.addEventListener("click", drawingFunction))
+    console.log(square)
+    function drawingFunction() {
+        console.log(event.tr)
+        event.target.style.backgroundColor = "black"
 
-
-
-
-
-
-console.log("hi!")
+    }
